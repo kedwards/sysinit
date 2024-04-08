68c53b8b66dc6e535401aac751830e136a94baef
kedwards <kedwards@kevinedwards.ca>
2024-04-08 07:38:52 -0600

    more cleanup, and added mise and starship

--- install.sh
+++ install.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
+SYSINIT_PATH=$HOME/sysinit
+
 trap cleanup ERR EXIT
 
 cleanup() {
   if command -v deactivate &> /dev/null; then
   	deactivate
- 	rm -fr $HOME/.sysinit
+    rm -fr $HOME/.sysinit
   fi
 }
 
-SYSINIT_PATH=$HOME/sysinit
-RELEASE=$(cat /etc/os-release | grep '^ID=' | awk '{ split($0, a, "="); print a[2]}')
-CODENAME=$(lsb_release -cs)
-
 (sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y) || exit 1
 
-sudo apt install git python3-venv -y
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
-if [ ! -d $HOME/.sysinit ]; then
-  python3 -m venv $HOME/.sysinit
+uv venv $HOME/.sysinit
+source $HOME/.sysinit/bin/activate
+
+if [ -d "${SYSINIT_PATH}" ]; then
+  git -C "${SYSINIT_PATH}" pull
+else
+  git clone -b main --single-branch https://github.com/kedwards/sysinit.git "$SYSINIT_PATH"
 fi
-. $HOME/.sysinit/bin/activate
 
-[ -d "${SYSINIT_PATH}" ] || git clone -b main --single-branch https://github.com/kedwards/sysinit.git "$SYSINIT_PATH"
-
-pip install -r "${SYSINIT_PATH}/requirements.txt"
+uv pip install -r "${SYSINIT_PATH}/requirements.txt"
 
 ansible-playbook "${SYSINIT_PATH}/playbook.yml" -K --ask-vault-pass --tags system
-
