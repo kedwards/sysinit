f24d80fb1536a5bd0dac43bf2e1e52e2a9c3ebf3
kedwards <kedwards@kevinedwards.ca>
2024-12-25 11:40:00 -0700

    January rebuff

--- install.sh
+++ install.sh
@@ -6,24 +6,47 @@ packages="curl git"
 trap cleanup ERR EXIT
 
 cleanup() {
-  if command -v deactivate &> /dev/null; then
+  if command -v deactivate &>/dev/null; then
     deactivate
   fi
 }
 
-. /etc/os-release
+function getPackageManager() {
+  local pm;
 
-if [ "$ID" = "debian" -o "$ID" = "ubuntu" ];
-then
-  sudo sh -c "apt-get update; apt-get upgrade -y; apt-get install $packages -y; apt autoremove -y"
-elif [ "$ID" = "arch" ]; then
-  sudo sh -c "echo 'yes' | pacman -S $packages"
-elif [ "$ID" = "rocky" ]; then
-  sudo dnf install -y $packages
-fi
+  declare -A osInfo;
+  osInfo[/etc/redhat-release]=yum
+  osInfo[/etc/arch-release]=pacman
+  osInfo[/etc/debian_version]=apt-get
 
-command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
-source "$HOME/.cargo/env"
+  for f in "${!osInfo[@]}"
+  do
+    if [[ -f $f ]]; then
+      pm="${osInfo[$f]}"
+    fi
+  done
+  echo "$pm"
+}
+
+case $(getPackageManager) in
+  "apt-get")
+    sudo sh -c "apt-get update; apt-get upgrade -y; apt-get install $packages -y; apt autoremove -y"
+    ;;
+  "pacman")
+    sudo sh -c "pacman -Syu; echo 'yes' | pacman -S --noconfirm $packages"
+    ;;
+  "yum")
+    sudo ish -c "yum update; yum install -y $packages"
+    ;;
+  *)
+    echo "Unsupported package manager"
+    exit 1
+    ;;
+esac
+
+curl https://mise.run | sh
+eval "$(~/.local/bin/mise activate bash)"
+mise use --global uv
 
 if [ ! -d "$HOME/.venv/sysinit" ]; then
   uv venv "$HOME/.venv/sysinit"
@@ -39,9 +62,4 @@ fi
 uv pip install -r "${sysinit_path}/requirements.txt"
 
 cd "${sysinit_path}" || exit 1
-ansible-playbook playbook.yml -K --ask-vault-pass
-
-if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  mkdir -p "$HOME/.tmux/plugins"
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
+ansible-playbook playbook.yml -k --ask-vault-pass
